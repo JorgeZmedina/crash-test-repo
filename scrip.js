cd4b3b5f3e0df89711d009cc2d80c17f037dfbe1
Jorge Medina <Jorge@Administrators-MacBook-Pro.local>
2023-02-22 16:02:48 -0500

    more file stuff

--- scrip.js
+++ scrip.js
@@ -5,5 +5,14 @@ function add(a, b) {
     return a + b;
 }
 
+function andre (a, b) {
+    let i=0
+    while (i<1000){
+    console.log(`Andres is mid`);
+    i++
+    }
+}
+
 console.log(add(a,b));
 console.log(a + b);
+console.log(andre(a, b))
